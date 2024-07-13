62933ee133aaf0084d9071359031043dae4853f9
Matthias Watermann <support@mwat.de>
2024-07-13 12:26:47 +0200

    + initial checkin;

---
+++ sortedmap.go
@@ -0,0 +1,354 @@
+/*
+Copyright © 2023, 2024  M.Watermann, 10247 Berlin, Germany
+
+		All rights reserved
+	EMail : <support@mwat.de>
+*/
+package hashtags
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"sort"
+	"sync"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+// `TSortedMap` is a generic type that accepts two type parameters:
+// - K for the key type (which must be cmp.Ordered)
+// - V for the value type
+//
+// The `cmp.Ordered` interface is defined as:
+//
+//	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+//		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+//		~float32 | ~float64 |
+//		~string
+//
+// `comparable` is an interface that is implemented by all comparable
+// types (booleans, numbers, strings, pointers, channels, arrays of
+// comparable types, structs whose fields are all comparable types).
+//
+// All methods are optionally thread-safe and can be called concurrently.
+type TSortedMap[K cmp.Ordered, V comparable] struct {
+	data map[K]V
+	keys []K
+	mtx  sync.RWMutex
+	safe bool
+}
+
+// --------------------------------------------------------------------------
+// constructor function
+
+// `NewSortedMap()` creates a new instance of `TSortedMap` with the
+// specified key and value types.
+//
+// The returned map is initially empty.
+//
+// Parameters:
+//   - `K`: The type of the keys in the sorted map.
+//   - `V`: The type of the values in the sorted map.
+//   - `aSafe`: Flag to decide whether the returned map should be
+//     thread safe, i.e. use a `sync.RWMutex` in all methods.
+//
+// Returns:
+// - `*TSortedMap[K, V]`: A pointer to a new instance with the given
+// key and value types.
+func NewSortedMap[K cmp.Ordered, V comparable](aSafe bool) *TSortedMap[K, V] {
+	return &TSortedMap[K, V]{
+		data: make(map[K]V),
+		keys: make([]K, 0),
+		safe: aSafe,
+	}
+} // NewSortedMap()
+
+// --------------------------------------------------------------------------
+// methods of TSortedMap
+
+// `Clear()` empties the internal data structures:
+// all map entrie are deleted.
+//
+// Returns:
+// - `*TSortedMap`: The cleared hash map.
+func (sm *TSortedMap[K, V]) Clear() *TSortedMap[K, V] {
+	if sm.safe {
+		sm.mtx.Lock()
+		defer sm.mtx.Unlock()
+	}
+
+	sm.data = make(map[K]V)
+	sm.keys = make([]K, 0)
+
+	return sm
+} // Clear()
+
+func (sm *TSortedMap[K, V]) delete(aKey K) bool {
+	// Check if the key actually exists
+	if _, exists := sm.data[aKey]; exists {
+		delete(sm.data, aKey)
+
+		// Update the keys slice
+		for idx, key := range sm.keys {
+			if key == aKey {
+				sm.keys = append(sm.keys[:idx], sm.keys[idx+1:]...)
+				break
+			}
+		}
+		return true
+	}
+
+	return false
+} // delete()
+
+// `Delete()` removes a key/value pair from the map.
+//
+// Parameters:
+// - `aKey`: The key of the entry to be deleted.
+//
+// Returns:
+// - `bool`: `true` if `aKey` was removed, or `false` otherwise.
+func (sm *TSortedMap[K, V]) Delete(aKey K) bool {
+	if sm.safe {
+		sm.mtx.Lock()
+		defer sm.mtx.Unlock()
+	}
+
+	return sm.delete(aKey)
+} // Delete()
+
+// - `[]K`: The index of `aID` in the list.
+func (sm *TSortedMap[K, V]) findIndex(aValue V) []K {
+	var result []K
+
+	for _, key := range sm.keys {
+		if sm.data[key] == aValue {
+			result = append(result, key)
+		}
+	}
+
+	return result
+} // findIndex()
+
+// `FindIndex()` returns a slice of keys that have the given value.
+//
+// Parameters:
+// - `aValue`: The element to look up.
+//
+// Returns:
+// - `[]K`: The index of `aID` in the list.
+func (sm *TSortedMap[K, V]) FindIndex(aValue V) []K {
+	if sm.safe {
+		sm.mtx.RLock()
+		defer sm.mtx.RUnlock()
+	}
+
+	return sm.findIndex(aValue)
+} // FindIndex()
+
+// `Get()` retrieves a value by its key from the SortedMap
+//
+// Parameters:
+// - `aKey`: The key of the entry to be retrieved.
+//
+// Returns:
+// - `V`: The value associated with the `aKey`.
+// - `bool`: An indication whether the key was found in the map.
+func (sm *TSortedMap[K, V]) Get(aKey K) (V, bool) {
+	if sm.safe {
+		sm.mtx.RLock()
+		defer sm.mtx.RUnlock()
+	}
+
+	value, exists := sm.data[aKey]
+
+	return value, exists
+} // Get()
+
+// Keys returns a slice of all keys in sorted order
+
+// `Keys()` returns a slice of all keys in sorted order
+//
+// This method returns a slice of all keys in the map in sorted order.
+//
+// Returns:
+// - `[]K`: A slice of keys in the sorted map.
+func (sm *TSortedMap[K, V]) Keys() []K {
+	if sm.safe {
+		sm.mtx.RLock()
+		defer sm.mtx.RUnlock()
+	}
+
+	return append([]K{}, sm.keys...)
+} // Keys()
+
+func (sm *TSortedMap[K, V]) insert(aKey K, aValue V) bool {
+	if _, exists := sm.data[aKey]; exists {
+		sm.data[aKey] = aValue
+
+		return true
+	}
+
+	// There are different situations to consider:
+	// 1: the key-list is empty,
+	// 2: the key-list doesn't already contain the key,
+	// 3: the key-list contains the key but with a different value
+	sLen := len(sm.keys)
+	if 0 == sLen {
+		// 1: empty list: just add the new item
+		sm.keys = append(sm.keys, aKey)
+	} else {
+		// find the insertion index using binary search
+		idx := sort.Search(sLen, func(i int) bool {
+			return sm.keys[i] >= aKey
+		})
+
+		if sLen == idx {
+			// 2: key not found: add key at the end
+			sm.keys = append(sm.keys, aKey)
+		} else if (sm.keys)[idx] != aKey {
+			// 3: the search index doesn't point to the required key
+			sm.keys = append(sm.keys, aKey)
+			copy((sm.keys)[idx+1:], (sm.keys)[idx:])
+			(sm.keys)[idx] = aKey
+		} else {
+			// dummy instruction for debugger
+			sLen = 0
+		}
+	}
+	sm.data[aKey] = aValue
+
+	return true
+} // Insert()
+
+// `Insert()` adds or updates a key/value pair in the sorted map.
+//
+// Parameters:
+// - `aKey`: The key of the entry to be added or updated.
+// - `aValue`: The value to be associated with the key.
+//
+// Returns:
+// - `bool`: `true` if `aID` was inserted, or `false` otherwise.
+func (sm *TSortedMap[K, V]) Insert(aKey K, aValue V) bool {
+	if sm.safe {
+		sm.mtx.Lock()
+		defer sm.mtx.Unlock()
+	}
+
+	return sm.insert(aKey, aValue)
+} // Insert()
+
+// `Iterate()` allows iteration over the map in sorted key order.
+//
+// Parameters:
+// - `f`: A function that takes a key and its associated value as
+// arguments and performs some operation on them.
+//
+// Returns:
+// - `*TSortedMap[K, V]`: A pointer to the same SortedMap instance,
+// allowing method chaining.
+func (sm *TSortedMap[K, V]) Iterate(f func(K, V)) *TSortedMap[K, V] {
+	if sm.safe {
+		sm.mtx.RLock()
+		defer sm.mtx.RUnlock()
+	}
+
+	for _, key := range sm.keys {
+		f(key, sm.data[key])
+	}
+
+	return sm
+} // Iterate()
+
+func (sm *TSortedMap[K, V]) Iterator() func() (K, V, bool) {
+	var idx int
+
+	return func() (K, V, bool) {
+		var (
+			key K
+			val V
+		)
+		if idx < len(sm.keys) {
+			key = sm.keys[idx]
+			val = sm.data[key]
+			idx++ // used from outer closure
+
+			return key, val, true
+		}
+
+		return key, val, false
+	}
+} // Iterator()
+
+func (sm *TSortedMap[K, V]) rename(aOldKey, aNewKey K) bool {
+	// Check if the new key already exists
+	if _, exists := sm.data[aNewKey]; exists {
+		return false
+	}
+
+	// Check if the old key exists
+	oldValue, exists := sm.data[aOldKey]
+	if !exists || (aOldKey == aNewKey) {
+		return false
+	}
+
+	// Remove the old key and add the new key
+	delete(sm.data, aOldKey)
+	sm.data[aNewKey] = oldValue
+
+	// Update the keys slice
+	for idx, key := range sm.keys {
+		if key == aOldKey {
+			sm.keys[idx] = aNewKey
+			break
+		}
+	}
+
+	// Re-sort the keys
+	slices.Sort(sm.keys) // ascending
+
+	return true
+} // Rename()
+
+// `Rename()` changes the key of an existing entry without affecting its value.
+//
+// If `aOldKey` equals `aNewKey`, or aOldKey` doesn't exist then they are
+// silently ignored (i.e. this method does nothing), returning `false`.
+//
+// If `aNewKey` already exists, it is ignored and the method returns `false`.
+//
+// Parameters:
+// - `aOldKey`: the key to be replaced in this list.
+// - `aNewKey`: The replacement key in this list.
+//
+// Returns:
+// - `bool`: `true` if the the renaming was successful, or `false` otherwise.
+func (sm *TSortedMap[K, V]) Rename(aOldKey, aNewKey K) bool {
+	if sm.safe {
+		sm.mtx.Lock()
+		defer sm.mtx.Unlock()
+	}
+
+	return sm.rename(aOldKey, aNewKey)
+} // Rename()
+
+func (sm *TSortedMap[K, V]) string() (rStr string) {
+	// Access items in sorted order:
+	iter := sm.Iterator()
+	for key, value, hasNext := iter(); hasNext; key, value, hasNext = iter() {
+		rStr += fmt.Sprintf("[%v]\n%v\n", key, value)
+	}
+	return
+} // string()
+
+func (sm *TSortedMap[K, V]) String() string {
+	if sm.safe {
+		sm.mtx.RLock()
+		defer sm.mtx.RUnlock()
+	}
+
+	return sm.string()
+} // String()
+
+/* EoF */
---
+++ sortedslice.go
@@ -0,0 +1,342 @@
+/*
+Copyright ©  2024  M.Watermann, 10247 Berlin, Germany
+
+		All rights reserved
+	EMail : <support@mwat.de>
+*/
+package hashtags
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+type (
+	// `TSortedSlice` represents a sorted slice of any ordered type.
+	//
+	// This is a generic type that accepts a type parameter:
+	// - T for the ordered value type.
+	//
+	// The `Ordered` interface is defined as:
+	// 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+	// 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+	// 		~float32 | ~float64 |
+	// 		~string
+	//
+	// All methods are optionally thread-safe and can be called concurrently.
+	TSortedSlice[T cmp.Ordered] struct {
+		data []T
+		mtx  sync.RWMutex
+		safe bool
+	}
+)
+
+// --------------------------------------------------------------------------
+// constructor function
+
+// `NewSortedSlice()` creates a new `TSortedSlice`.
+//
+// If the given `aList` is empty the initial capacity of the underlying
+// list is set to 32 to optimise memory usage.
+//
+// Parameters:
+// - `aList`: The slice to use with the sorted slice.
+// - `aSafe`: Flag to decide whether the returned map should be
+// thread safe, i.e. use a `sync.RWMutex` in all methods.
+//
+// Returns:
+// - `*TSortedSlice[T]`: A pointer to the newly created instance.
+func NewSortedSlice[T cmp.Ordered](aList []T, aSafe bool) *TSortedSlice[T] {
+	var list []T
+
+	if 0 < len(aList) {
+		list = aList
+	} else {
+		list = make([]T, 0, 32)
+	}
+
+	ss := &TSortedSlice[T]{
+		data: list,
+		safe: aSafe,
+	}
+	slices.Sort(ss.data)
+
+	return ss
+} // NewSortedSlice()
+
+// -------------------------------------------------------------------------
+// methods of TSortedSlice
+
+// `Clear()` removes all entries in this list.
+//
+// Returns:
+// - `*TSortedSlice[T]`: The cleared list instance.
+func (ss *TSortedSlice[T]) Clear() *TSortedSlice[T] {
+	if ss.safe {
+		ss.mtx.Lock()
+		defer ss.mtx.Unlock()
+	}
+
+	if nil != ss {
+		ss.data = make([]T, 0, 32)
+	}
+
+	return ss
+} // Clear()
+
+func (ss *TSortedSlice[T]) delete(aElement T) bool {
+	sLen := len(ss.data)
+	if 0 == sLen { // empty list
+		return false
+	}
+
+	// Find the index of the given element in the sorted slice
+	idx, ok := slices.BinarySearch(ss.data, aElement)
+	if !ok {
+		return false
+	}
+
+	if (idx < sLen) && (ss.data[idx] == aElement) {
+		// `aElement` found at index `idx`
+		if 0 == idx {
+			if 1 == sLen { // the only element
+				ss.data = make([]T, 0, 32)
+			} else { // a longer list
+				ss.data = ss.data[1:] // remove the first element
+			}
+		} else if (sLen - 1) == idx { // remove the last element
+			ss.data = ss.data[:idx]
+		} else { // remove element in the middle
+			ss.data = append(ss.data[:idx], ss.data[idx+1:]...)
+		}
+		return true
+	}
+
+	return false
+} // delete()
+
+// `Delete()` removes an element from the sorted slice.
+//
+// Parameters:
+// - `aElement`: The element to remove from the list.
+//
+// Returns:
+// - `bool`: `true` if `aElement` was removed, or `false` otherwise.
+func (ss *TSortedSlice[T]) Delete(aElement T) bool {
+	if ss.safe {
+		ss.mtx.Lock()
+		defer ss.mtx.Unlock()
+	}
+
+	return ss.delete(aElement)
+} // Delete()
+
+// `Data()` returns the underlying data of the sorted slice.
+//
+// Returns:
+// - `[]T`: The underlying data of the sorted slice.
+func (ss *TSortedSlice[T]) Data() []T {
+	if ss.safe {
+		ss.mtx.RLock()
+		defer ss.mtx.RUnlock()
+	}
+
+	return append([]T{}, ss.data...)
+} // Data()
+
+func (ss *TSortedSlice[T]) findIndex(aElement T) int {
+	sLen := len(ss.data)
+	if 0 == sLen {
+		return -1
+	}
+
+	// Find the index of the given element
+	idx, exists := slices.BinarySearch(ss.data, aElement)
+	if !exists {
+		return -1
+	}
+
+	if idx < sLen && ss.data[idx] == aElement {
+		return idx
+	}
+
+	return -1 // aElement not found
+} // findIndex()
+
+// `FindIndex()` returns the list index of `aElement`.
+//
+// If the `aElement` is not found, the method returns -1.
+//
+// Parameters:
+// - `aElement`: The list element to look up.
+//
+// Returns:
+// - `int`: The index of `aID` in the list.
+func (ss *TSortedSlice[T]) FindIndex(aElement T) int {
+	if ss.safe {
+		ss.mtx.RLock()
+		defer ss.mtx.RUnlock()
+	}
+
+	return ss.findIndex(aElement)
+} // FindIndex()
+
+// `Get()` retrieves a value by its list index from the sorted slice.
+//
+// Parameters:
+// - `aIndex`: The list index to use for returning the list element.
+//
+// Returns:
+// - `T`: The value associated with the `aIndex`.
+// - `bool`: An indication whether the index was found in the list.
+func (ss *TSortedSlice[T]) Get(aIndex int) (T, bool) {
+	if ss.safe {
+		ss.mtx.RLock()
+		defer ss.mtx.RUnlock()
+	}
+	var result T // variable with its zero value
+
+	if aIndex < len(ss.data) {
+		return ss.data[aIndex], true
+	}
+
+	return result, false
+} // Get()
+
+func (ss *TSortedSlice[T]) insert(aElement T) bool {
+	sLen := len(ss.data)
+	if 0 == sLen { // empty list
+		ss.data = append(ss.data, aElement)
+
+		return true
+	}
+
+	// find the insertion index using binary search
+	idx, exists := slices.BinarySearch(ss.data, aElement)
+	if !exists {
+		ss.data = append(ss.data, aElement)
+
+		return true
+	}
+
+	if sLen == idx { // key not found
+		ss.data = append(ss.data, aElement) // add new element
+		return true
+	}
+	if ss.data[idx] != aElement {
+		ss.data = append(ss.data, aElement) // make room for new element
+		copy(ss.data[idx+1:], ss.data[idx:])
+		ss.data[idx] = aElement
+
+		return true
+	}
+
+	return false
+} // insert()
+
+// `Insert()` adds an element to the sorted slice while maintaining order.
+//
+// Parameters:
+// - `aElement` The element to insert to the list.
+//
+// Returns:
+// - `bool`: `true` if `aElement` was inserted, or `false` otherwise.
+func (ss *TSortedSlice[T]) Insert(aElement T) bool {
+	if ss.safe {
+		ss.mtx.Lock()
+		defer ss.mtx.Unlock()
+	}
+
+	return ss.insert(aElement)
+} // Insert()
+
+func (ss *TSortedSlice[T]) rename(aOldValue, aNewValue T) bool {
+	if (0 == len(ss.data)) || (aOldValue == aNewValue) {
+		return false
+	}
+
+	idx := ss.findIndex(aOldValue)
+	if 0 > idx { // ID not found
+		return ss.insert(aNewValue)
+	}
+
+	if !ss.insert(aNewValue) {
+		// This should only happen it there's an OOM problem.
+		// Hence we just replace the aOldValue by aNewValue and
+		// sort the list again.
+		if ss.data[idx] != aNewValue {
+			ss.data[idx] = aNewValue
+			slices.Sort(ss.data) // ascending
+			return true
+		}
+		return false
+	}
+
+	return ss.delete(aOldValue)
+} // rename()
+
+// Rename changes an element in the sorted slice and maintains order.
+func (ss *TSortedSlice[T]) Rename(aOldValue, aNewValue T) bool {
+	if ss.safe {
+		ss.mtx.Lock()
+		defer ss.mtx.Unlock()
+	}
+
+	return ss.rename(aOldValue, aNewValue)
+} // Rename()
+
+func (ss *TSortedSlice[T]) string() string {
+	if 0 == len(ss.data) {
+		return "[]"
+	}
+
+	var builder strings.Builder
+	// Here we ignore the return values of the `builder` functions
+	// since we can't do anything about them anyway.
+	builder.WriteString("[")
+	for idx, elem := range ss.data {
+		if 0 < idx {
+			builder.WriteString(", ")
+		}
+		builder.WriteString(fmt.Sprintf("%v", elem))
+	}
+	builder.WriteString("]")
+
+	return builder.String()
+} // string()
+
+// `String()` implements the `fmt.Stringer` interface.
+//
+// For each element, the method appends the element's string
+// representation while elements are separated by ", ".
+//
+// Returns:
+// - `string`: The list's contents as a string.
+func (ss *TSortedSlice[T]) String() string {
+	if ss.safe {
+		ss.mtx.RLock()
+		defer ss.mtx.RUnlock()
+	}
+
+	return ss.string()
+} // String()
+
+// func main() {
+// 	// Example usage
+// 	ints := NewSortedSlice([]int{5, 3, 4, 1, 2})
+// 	ints.Insert(6)
+// 	fmt.Println(ints.Data()) // Output: [1 2 3 4 5 6]
+
+// 	ints.Remove(3)
+// 	fmt.Println(ints.Data()) // Output: [1 2 4 5 6]
+
+// 	ints.Rename(4, 7)
+// 	fmt.Println(ints.Data()) // Output: [1 2 5 6 7]
+// }
+
+/* EoF */
